fb31ccd950c4dee28a648e1f28d1795a4ebf936d
pedroakechan <pedroakechan321@gmail.com>
2026-06-18 18:59:45 -0600

    client con url nueva

--- src/api/client.js
+++ src/api/client.js
@@ -1,9 +1,6 @@
 import axios from "axios";
 
 export const STORAGE_URL = "https://back-eatup.onrender.com";
-{/*
-VITE_STORAGE_URL=https://back-eatup.onrender.com
-*/}
 
 const api = axios.create({
   baseURL: "https://back-eatup.onrender.com",
